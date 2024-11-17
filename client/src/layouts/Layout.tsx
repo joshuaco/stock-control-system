3f36a0c696576e3a4e8c224bb28d5db8dc07d3ac
Joshua <joxuecont@gmail.com>
2024-11-17 07:46:16 -0400

    client page first commit: basic layout

---
+++ client/src/layouts/Layout.tsx
@@ -0,0 +1,21 @@
+import { Outlet } from 'react-router-dom';
+
+function Layout() {
+  return (
+    <>
+      <header className='bg-slate-800'>
+        <div className='mx-auto max-w-6xl py-6'>
+          <h1 className='text-4xl font-bold text-white'>
+            Stock Control System
+          </h1>
+        </div>
+      </header>
+
+      <main className='mx-auto mt-10 max-w-6xl p-10 bg-white shadow'>
+        <Outlet />
+      </main>
+    </>
+  );
+}
+
+export default Layout;
