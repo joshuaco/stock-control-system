06f1ad5db5cb6e6c25d8e686973818644b64cc00
Joshua <joxuecont@gmail.com>
2024-11-23 13:29:57 -0400

    add morgan log middleware

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { connectDB } from './config/db';
 import cors, { CorsOptions } from 'cors';
+import morgan from 'morgan';
 import swaggerUi from 'swagger-ui-express';
 import swaggerSpec, { swaggerUIoptions } from './config/swagger';
 import productsRouter from './routes/router';
@@ -19,8 +20,9 @@ const corsOptions: CorsOptions = {
 server.use(cors(corsOptions));
 
 server.use(express.json());
-server.use('/api/products', productsRouter);
+server.use(morgan('dev'));
 
+server.use('/api/products', productsRouter);
 server.use('/api', (_req, res) => {
   res.status(200).json({ message: 'API is running' });
 });
