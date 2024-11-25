e1506cc74cfd8a295a9673d41ea0119418961779
Joshua <joxuecont@gmail.com>
2024-11-24 22:58:14 -0400

    add product validation

--- client/src/loaders/productsLoader.ts
+++ client/src/loaders/productsLoader.ts
@@ -3,9 +3,14 @@ import { getProducts } from '../services/ProductService';
 export async function loader() {
   try {
     const products = await getProducts();
+
+    if (!products) {
+      throw new Error('No products found');
+    }
+
     return products;
   } catch (error) {
     console.log(error);
-    throw new Error('Error loading products');
+    throw new Response('Error loading products', { status: 500 });
   }
 }
